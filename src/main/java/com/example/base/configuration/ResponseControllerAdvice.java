211c947e0c7d044cf4d855a1e90d779404758267
zhangjiwei1 <zhangjiwei1@gome.com.cn>
2020-05-07 15:15:04 +0800

    优化

---
+++ src/main/java/com/example/base/configuration/ResponseControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.example.base.configuration;
+
+import com.example.base.vo.ResponseVO;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice(basePackages = {"com.example.base.controller"})
+public class ResponseControllerAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> aClass) {
+        // 如果接口返回的类型本身就是ResultVO那就没有必要进行额外的操作，返回false
+        return !returnType.getGenericParameterType().equals(ResponseVO.class);
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object data, MethodParameter returnType, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        // string 类型不能包装，所以要进行特殊处理
+        if (returnType.getGenericParameterType().equals(String.class)) {
+
+        }
+        return ResponseVO.success(data);
+    }
+}
--- src/main/java/com/example/base/controller/HelloController.java
+++ src/main/java/com/example/base/controller/HelloController.java
@@ -1,15 +1,19 @@
 package com.example.base.controller;
 
+import com.example.base.entity.UserOrgRelation;
+import com.example.base.vo.ResponseVO;
+import org.apache.commons.compress.utils.Lists;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 测试接口
@@ -25,6 +29,60 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "hello";
+    }
+
+    @GetMapping("/string")
+    public String sayString() {
+        return "string";
+    }
+
+    @GetMapping("/int")
+    public int sayInt() {
+        return 1;
+    }
+
+    @GetMapping("/boolean")
+    public boolean sayBoolean() {
+        return true;
+    }
+
+    @GetMapping("/long")
+    public long sayLong() {
+        return 5L;
+    }
+
+    @GetMapping("/vo")
+    public ResponseVO sayVo() {
+        return ResponseVO.success(56);
+    }
+
+    @GetMapping("/exception")
+    public long sayException() {
+        int num = ThreadLocalRandom.current().nextInt(2, 8);
+        if (num < 5) {
+            throw new RuntimeException();
+        }
+        return 5L;
+    }
+
+    @GetMapping("/getUser")
+    public UserOrgRelation getUser() {
+        UserOrgRelation relation = new UserOrgRelation();
+        relation.setId(1);
+        relation.setUserId("A454");
+        relation.setOrgId("Org78");
+        return relation;
+    }
+
+    @GetMapping("/getUsers")
+    public List<UserOrgRelation> getUsers() {
+        List<UserOrgRelation> relationList = Lists.newArrayList();
+        UserOrgRelation relation = new UserOrgRelation();
+        relation.setId(1);
+        relation.setUserId("A454");
+        relation.setOrgId("Org78");
+        relationList.add(relation);
+        return relationList;
     }
 
     @GetMapping("/date")
